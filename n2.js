902053819615fd631f2baf4a195bb717bb22bfe3
Jitin Kumar <jk9120810343@gmail.com>
2025-07-25 02:04:08 +0530

    Update n2.js

--- n2.js
+++ n2.js
@@ -523,8 +523,7 @@ class PortfolioEnhancer {
     }
 }
 function downloadResume() {
-    window.open("https://drive.google.com/file/d/1EnCCqvl6tkXEl497JhgnO7laGZVTbqYN/view?usp=sharing","_blank")
-
+  window.open("https://drive.google.com/uc?export=download&id=1EnCCqvl6tkXEl497JhgnO7laGZVTbqYN", "_blank");
 }
 
 window.portfolioEnhancer = new PortfolioEnhancer();
